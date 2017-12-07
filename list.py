c532dc778407ad18c2cd8c6e087b6e8a52172769
SOLANKE Ritesh <XQI@dsone.3ds.com>
2017-12-07 11:43:42 +0530

    Added for hello

---
+++ list.py
@@ -0,0 +1,16 @@
+# list = ['spatial','solidworks','catia'];
+# print "list[0]", list[0]
+# print "list[0:2]",list[1:4]
+# list[0]='interop'
+# print "list[0]", list[0]
+# del list[2]
+# print "list[0:2]",list[0:4]
+# print len(list)
+# print 'spatial' in list
+# print 'interop' in list
+
+numlist = [2,1,3,0,6];
+print numlist[0:5]
+numlist.reverse()
+print numlist[0:5]
+print("hello")
