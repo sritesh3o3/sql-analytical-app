166d568fe7f48075b9bac61860bf899522886df1
SOLANKE Ritesh <ritesh.solanke@gmail.com>
2017-12-07 12:15:13 +0530

    Added for forloop

---
+++ forloop.py
@@ -0,0 +1,13 @@
+
+list =("spatial", "enovia", "catia")
+sports =("cricket", "football", "baseball")
+count = 1
+for brand in list:
+     print(count)
+     print(brand)
+     count+=1
+     if count == 3:
+      break
+print('\n')     
+for sport in sports:
+     print(sport)     
